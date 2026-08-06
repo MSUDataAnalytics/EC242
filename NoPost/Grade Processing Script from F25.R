a64cbb80a8181acbe8ca99d6623ce0bd5ef07db5
Justin Kirkpatrick <aubreyjkirkpatrick@gmail.com>
2026-08-06 10:12:46 -0400

    Last year-end push.

--- NoPost/Grade Processing Script from F25.R
+++ NoPost/Grade Processing Script from F25.R
@@ -2,14 +2,18 @@
 
 
 
-gr = fread("C:\\Users\\jkirk\\OneDrive - Michigan State University\\Teaching\\EC242_FS25 Grading\\Final Grades\\FS25-EC-242-001 - Social Science Data Analytics_GradesExport_2025-12-15-19-17 AS SUBMITTED.csv")
+# F26 gr = fread("C:\\Users\\jkirk\\OneDrive - Michigan State University\\Teaching\\EC242_FS25 Grading\\Final Grades\\FS25-EC-242-001 - Social Science Data Analytics_GradesExport_2025-12-15-19-17 AS SUBMITTED.csv")
 
+# S26 gr = fread("C:\\Users\\jkirk\\OneDrive - Michigan State University\\Teaching\\EC242_SS26 Grading\\Final Grades\\SS26-EC-242-001 - Social Science Data Analytics_GradesExport_2026-05-05-14-09.csv")
 
-
-gr = gr[,score:=100*`Calculated Final Grade Numerator`/`Calculated Final Grade Denominator`]
+gr = gr[,scoreUnround:=100*`Calculated Final Grade Numerator`/`Calculated Final Grade Denominator`]
 gr = gr[,score:=100*round(`Calculated Final Grade Numerator`/`Calculated Final Grade Denominator`,2)]
-gr = gr[,c(1,3,4,8)]
-gr[,grade:=case_when(score>=94 ~ 4.0, # NEEDS TO UPDATE FOR S26
+gr = gr[,.(OrgDefinedId, `Last Name`, `First Name`, scoreUnround, score)]
+
+# gr = gr[`First Name` !='Demo',]
+
+
+gr[,grade:=case_when(score>=95 ~ 4.0, # Update here (check syllabus)
                      score>=88 ~ 3.5,
                      score>=83 ~ 3.0,
                      score>=77 ~ 2.5,
@@ -17,9 +21,14 @@ gr[,grade:=case_when(score>=94 ~ 4.0, # NEEDS TO UPDATE FOR S26
                      score>=66 ~ 1.5,
                      score>=58 ~ 1.0,
                      score<=58.00 ~ 0.0)]
+gr[`Last Name`=='Jain', grade:=4.0]  # S26, was on bubble, allowed extra Weekly Writing
 
-grunr = copy(gr)
+gr = gr[order(`Last Name`, `First Name`),]
+mean(gr$grade[gr$`Last Name`!='Student'])
 
+
+
+write_csv(gr, file.path("C:\\Users\\jkirk\\OneDrive - Michigan State University\\Teaching\\EC242_SS26 Grading\\Final Grades\\SS26-EC-242-001 - Social Science Data Analytics_GradesExport_2026-05-05-14-09 SUBMITTED.csv"))
 
 
 
